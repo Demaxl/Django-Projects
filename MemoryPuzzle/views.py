5037694dd211ecb68f0dfb898d47bdbabbddea6a
Abdullahi <aweabdullahi@gmail.com>
2022-09-15 19:47:29 +0100

    Changed project name back

--- MemoryPuzzle/views.py
+++ MemoryPuzzle/views.py
@@ -1,16 +1,26 @@
-import os
+import os, random
 
 from django.shortcuts import render
 from django.http import HttpResponse
 
 
 IMAGES_BASE_PATH = './MemoryPuzzle/static/MemoryPuzzle/images/'
+BOARDWIDTH = 6
+BOARDHEIGHT = 5
 
-board = []
-images_path = [path for path in os.listdir(IMAGES_BASE_PATH)]
+BOX_NUM = BOARDWIDTH * BOARDHEIGHT
+
+
+def prepare():
+    images_path = [path for path in os.listdir(IMAGES_BASE_PATH)]
+
+    random.shuffle(images_path)
+    images = images_path[:BOX_NUM]
+
+    return images
 
 def index(request):
-    print(images_path)
+
     return render(request, "MemoryPuzzle/index.html", {
-        "images": images_path
+        "images": prepare()
     })
